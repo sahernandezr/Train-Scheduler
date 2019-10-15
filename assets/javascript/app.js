ec885252016eaabf9c160717a9f5b53f2461be12
sahernandezr <[E50846950+sahernandezr@users.noreply.github.comgit config --global user.email 50846950+sahernandezr@users.noreply.github.com>
2019-10-15 18:24:17 -0500

    css changed

---
+++ assets/javascript/app.js
@@ -0,0 +1,12 @@
+// Your web app's Firebase configuration
+var firebaseConfig = {
+    apiKey: "AIzaSyCibHccsoOOucscr0E_0NOzXx0ZTqRxD7I",
+    authDomain: "train-table-ca472.firebaseapp.com",
+    databaseURL: "https://train-table-ca472.firebaseio.com",
+    projectId: "train-table-ca472",
+    storageBucket: "train-table-ca472.appspot.com",
+    messagingSenderId: "941351706429",
+    appId: "1:941351706429:web:1b7776804daa4dcea86f54"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
